710bdd35f0ecced695f365a8d9bc631a877a85b9
hristijan.slavkoski <kiko_slavkoski@yahoo.com>
2022-12-13 03:37:09 +0100

    Initial Commit

---
+++ Poll-System-Application-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poll-System-Application-iOS
+//
+//  Created by Hristijan Slavkoski on 12/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
